ca605ec5555c51bfc0d4e4e39c18dfacc949e4a3
Ludvig Dickman <ludvig@avidity.se>
2013-05-16 22:00:51 +0200

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Caraten::Application.config.secret_token = '7d302eaa137a68dd344cbf89671dec25a13d98ef18ef2ed20235cedb4045d1ca727daf101c9e13af2a9ae320d2271ed8a023a5f73236b0e681d7fe6ea3f51005'
