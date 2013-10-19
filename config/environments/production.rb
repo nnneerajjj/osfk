7c40bd31590ab3876c19ba424b2a3f11de480922
Deployer <deploy@luddement.(none)>
2013-10-19 19:34:40 +0200

    Fixed time zone

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -20,6 +20,7 @@ Caraten::Application.configure do
   # Generate digests for assets URLs
   config.assets.digest = true
 
+  Delayed::Worker.destroy_failed_jobs = false
   # Defaults to nil and saved in location specified by config.assets.prefix
   # config.assets.manifest = YOUR_PATH
 
