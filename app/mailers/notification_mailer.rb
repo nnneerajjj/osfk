31a6c00d5f49ab87fb94361e7ab42f323a81cf72
Ludvig Dickman <ludvig@avidity.se>
2013-10-27 12:53:49 +0100

    Fixed old comments notifications

--- app/mailers/notification_mailer.rb
+++ app/mailers/notification_mailer.rb
@@ -10,9 +10,18 @@ class NotificationMailer < ActionMailer::Base
     @news = News.where(["created_at > ?", date])
     @topics = Topic.where(["created_at > ?", date])
 
+    @comments = Comment.where(["created_at > ?", date])
     news_ids = @news.map(&:id)
     topic_ids = @topics.map(&:id)
-    @comments = Comment.where(["((commentable_type = 'Topic' and commentable_id not in (?)) or (commentable_type = 'News' and commentable_id not in (?))) and created_at > ?", topic_ids, news_ids, date])
+
+    if news_ids.any? and topic_ids.any?
+      @comments = Comment.where(["((commentable_type = 'Topic' and commentable_id not in (?)) or (commentable_type = 'News' and commentable_id not in (?)))", topic_ids, news_ids])
+    elsif topic_ids.any?
+      @comments = Comment.where(["(commentable_type = 'Topic' and commentable_id not in (?))", topic_ids])
+    elsif news_ids.any?
+      @comments = Comment.where(["(commentable_type = 'News' and commentable_id not in (?))", news_ids])
+    end
+
     commentables = @comments.group_by{|x| x.commentable}
 
     @old_news = commentables.select {|x| x.class == News}
