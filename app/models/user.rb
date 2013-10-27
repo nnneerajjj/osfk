6c8f027cf09e8198ad8d214dc90b22af80325a30
Ludvig Dickman <ludvig@avidity.se>
2013-10-27 13:36:53 +0100

    Added notiication script

--- app/models/user.rb
+++ app/models/user.rb
@@ -23,7 +23,7 @@ class User < ActiveRecord::Base
     "#{name} (#{street_number})"
   end
 
-  def send_notification
+  def send_notification!
     if self.notification_date.nil?
       self.update_attribute(:notification_date, Time.now)
       return
