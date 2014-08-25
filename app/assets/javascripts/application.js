5e7992882700845d8c21555e8fae7f523c007eaa
Ludvig Dickman <caterpiller@gmail.com>
2014-08-25 18:20:12 +0200

    Moved fonts and javascripts

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,6 +10,7 @@
 // WARNING: THE FIRST BLANK LINE MARKS THE END OF WHAT'S TO BE PROCESSED, ANY BLANK LINE SHOULD
 // GO AFTER THE REQUIRES BELOW.
 //
+//= require jquery-1.8.3.min
 //= require jquery.themepunch.tools.min
 //= require jquery.themepunch.revolution.min
 //= require jquery.sidr
