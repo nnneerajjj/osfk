0b2ce147a39dcb71fbab3f774213a31f6a0ea531
Ludvig Dickman <caterpiller@gmail.com>
2014-09-14 11:36:55 +0200

    Added edit page

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,12 +1,12 @@
 class RegistrationsController < Devise::RegistrationsController
   before_action :configure_permitted_parameters
 
-  def new
-    super
-    resource.regular = true
+  def create
+    result = super
+    flash[:alert] = resource.errors.full_messages.to_sentence
   end
 
-  def create
+  def update
     result = super
     flash[:alert] = resource.errors.full_messages.to_sentence
   end
@@ -15,6 +15,7 @@ class RegistrationsController < Devise::RegistrationsController
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.for(:sign_up) << [:firstname, :lastname, :telephone, :package1, :package2, :regular, :identity_number, :address, :postal_address ]
+    devise_parameter_sanitizer.for(:edit) << [:firstname, :lastname, :telephone, :identity_number, :address, :postal_address ]
   end
 
   def after_sign_up_path_for(resource)
