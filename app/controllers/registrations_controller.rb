e7d4ecbbe14e47a032e14c288cf141ad60361d3f
Ludvig Dickman <caterpiller@gmail.com>
2014-09-20 00:31:03 +0200

    Cleaned up forms

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,15 +1,41 @@
 class RegistrationsController < Devise::RegistrationsController
   before_action :configure_permitted_parameters
 
+  def index
+    redirect_to :edit
+  end
+
+  # PUT /resource
+  # We need to use a copy of the resource because we don't want to change
+  # the current user in place.
+  def update
+    self.resource = resource_class.to_adapter.get!(send(:"current_#{resource_name}").to_key)
+    @user = self.resource
+
+    successfully_updated = if needs_password?(@user, params)
+      @user.update_with_password(devise_parameter_sanitizer.sanitize(:account_update))
+    else
+      # remove the virtual current_password attribute
+      # update_without_password doesn't know how to ignore it
+      params[:user].delete(:current_password)
+      @user.update_without_password(devise_parameter_sanitizer.sanitize(:account_update))
+    end
+
+    if successfully_updated
+      flash[:notice] = "Din profil är uppdaterad"
+      # Sign in the user bypassing validation in case their password changed
+      sign_in @user, :bypass => true
+      redirect_to after_update_path_for(@user)
+    else
+      flash[:error] = resource.errors.full_messages.to_sentence
+      render "edit"
+    end
+  end
+
   def create
     result = super
     flash[:error] = resource.errors.full_messages.to_sentence
   end
-
-  # def update
-  #   result = super
-  #   flash[:alert] = resource.errors.full_messages.to_sentence
-  # end
 
   protected
 
@@ -23,6 +49,17 @@ class RegistrationsController < Devise::RegistrationsController
   end
 
   def after_update_path_for(resource)
-    main_app.news_index_path
+    edit_user_registration_path
+  end
+
+  private
+
+  # check if we need password to update user data
+  # ie if password or email was changed
+  # extend this as needed
+  def needs_password?(user, params)
+    user.email != params[:user][:email] ||
+      params[:user][:password].present? ||
+      params[:user][:password_confirmation].present?
   end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,18 +6,18 @@ class UsersController < ApplicationController
   end
 
   def update
-    if @user.update_attributes(params[:user])
-      if params[:user][:password].present?
-        flash[:info] = I18n.t(:password_changed)
-        sign_in @user, bypass: true
-      else
-        flash[:info] = I18n.t(:settings_changed)
-      end
+  #   if @user.update_attributes(params[:user])
+  #     if params[:user][:password].present?
+  #       flash[:info] = I18n.t(:password_changed)
+  #       sign_in @user, bypass: true
+  #     else
+  #       flash[:info] = I18n.t(:settings_changed)
+  #     end
 
-      redirect_to users_path, notice: "yes"
-    else
-      flash[:alert] = @user.errors.full_messages.to_sentence
-      render action: "index"
-    end
+  #     redirect_to users_path, notice: "yes"
+  #   else
+  #     flash[:alert] = @user.errors.full_messages.to_sentence
+  #     render action: "index"
+  #   end
   end
 end
