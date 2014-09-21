3c8087e70920befac8778c7637e01bfd194b6b28
Ludvig Dickman <caterpiller@gmail.com>
2014-09-21 03:19:01 +0200

    Removed pp

--- app/controllers/uploads_controller.rb
+++ app/controllers/uploads_controller.rb
@@ -2,7 +2,6 @@ class UploadsController < ApplicationController
   skip_before_filter :verify_authenticity_token
 
   def create
-    pp params
     upload = params[:upload]
     @upload = upload.keys.first.classify.constantize.new
     @upload.file = URI.parse(upload.values.first.gsub('%2F','/'))
