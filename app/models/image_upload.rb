c7ebff12cc011048d6e801b4d928621b837dc719
Ludvig Dickman <caterpiller@gmail.com>
2014-09-22 22:43:02 +0200

    Added missing style

--- app/models/image_upload.rb
+++ app/models/image_upload.rb
@@ -1,5 +1,5 @@
 class ImageUpload < Upload
-  has_attached_file :file, :styles => { :large => "1920x1200>", :medium => "830x550#", :small => "320x215#", :preview => "250x250#", :thumb => "80x80#" }
+  has_attached_file :file, :styles => { :large => "1920x1200>", :slider => "1920x660#", :medium => "830x550#", :small => "320x215#", :preview => "250x250#", :thumb => "80x80#" }
   validates_attachment_content_type :file, :content_type => /\Aimage\/.*\Z/
 
   attr_accessible :uploader_id, :uploader_type, :type, :uuid, :file
